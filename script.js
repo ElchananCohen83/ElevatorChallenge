54c270e1347e2efa8f8a5819325a3b29ebc7b563
ElchananCohen <elchanancohen83@gmail.com>
2024-06-26 23:13:20 +0300

    fix

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const building = document.getElementById('building');
 
-    const imgContainer1 = document.createElement('div');
-    imgContainer1.className = 'elevator-container1';
-    const img1 = document.createElement('img');
-    img1.src = './Public/elv.png';
-    img1.className = 'elevator-img1';
-    imgContainer1.appendChild(img1);
+    // Create elevator containers and images
+    const createElevator = (className, leftPosition) => {
+        const imgContainer = document.createElement('div');
+        imgContainer.className = className;
+        imgContainer.style.left = `${leftPosition}px`;
 
-    const imgContainer2 = document.createElement('div');
-    imgContainer2.className = 'elevator-container2';
-    const img2 = document.createElement('img');
-    img2.src = './Public/elv.png';
-    img2.className = 'elevator-img2';
-    imgContainer2.appendChild(img2);
+        const img = document.createElement('img');
+        img.src = './Public/elv.png';
+        img.className = 'elevator-img';
 
-    const imgContainer3 = document.createElement('div');
-    imgContainer3.className = 'elevator-container3';
-    const img3 = document.createElement('img');
-    img3.src = './Public/elv.png';
-    img3.className = 'elevator-img3';
-    imgContainer3.appendChild(img3);
+        imgContainer.appendChild(img);
+        building.appendChild(imgContainer);
+    };
 
-    building.appendChild(imgContainer1);
-    building.appendChild(imgContainer2);
-    building.appendChild(imgContainer3);
+    // Add three elevators
+    createElevator('elevator-container', 420);
+    createElevator('elevator-container', 540);
+    createElevator('elevator-container', 660);
 
+    // Create floors
     for (let i = 0; i < 25; i++) {
         const floor = document.createElement('div');
         floor.className = 'floor';
